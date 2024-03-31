6b54276ff656ab42de497b5d9bdd98a1cc4077a3
Raymond Baril <0x25cfb68@gmail.com>
2024-03-31 14:20:29 +0400

    fix export Call types

--- src/call/index.ts
+++ src/call/index.ts
@@ -1,3 +1,4 @@
+export type { Block, Call, EstimateOptions, ExecuteOptions } from './call.js';
 export { call, estimate, execute } from './call.js';
 export { multicall, Multicall } from './multicall.js';
 export { default as MulticallContracts, Deployment as MulticallDeployment } from './multicall.js';
