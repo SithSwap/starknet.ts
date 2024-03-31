da097822ed825b20b25f2c1a50df9927e52cd2ef
Raymond Baril <0x25cfb68@gmail.com>
2024-03-31 14:35:45 +0400

    fix starknetid exports

--- src/starknetid/index.ts
+++ src/starknetid/index.ts
@@ -1,4 +1,4 @@
-export { default as Deployment, StarknetID } from './deployment.js';
+export { default as Contracts, StarknetID, Deployment } from './deployment.js';
 export { decode, encode } from './codec.js';
 export { from, to, id } from './name.js';
 export { image, nft, uri } from './profile.js';
